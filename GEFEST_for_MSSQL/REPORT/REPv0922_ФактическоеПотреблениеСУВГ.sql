ad96e73c98120febe5fd9920b1b723afd946d743
Mikhail Chmir <Mchmir@mail.ru>
2022-09-24 00:36:15 +0600

    improvement(SQL) Фактическое потребление СУВГ через РУ
    - добавлено условие при выборке оставлять с 0 подключенные ОУ и ПУ
    - улучшен стиль

--- GEFEST_for_MSSQL/REPORT/REPv0922_ФактическоеПотреблениеСУВГ.sql
+++ GEFEST_for_MSSQL/REPORT/REPv0922_ФактическоеПотреблениеСУВГ.sql
@@ -1,4 +1,3 @@
-
 -- SETTING
 DECLARE @Potreblenie BIT;
 DECLARE @Month1_1    INT;
@@ -28,57 +27,55 @@ SET @Year2_1  = 2021;
 SET @Month2_2 = 6;
 SET @Year2_2  = 2022;
 --------------------------------------
-SET @Potreblenie = 0; -- с нулевым потреблением, 1 - без 0 в потрблении 
+-- с нулевым потреблением, оставляем 0 только для подключенных ОУ и ПУ
+-- 1 - без 0 в потрблении 
+SET @Potreblenie = 0; 
 
 --------------------------------------
 -- END SETTING
 
 
 --- PROGRAM
-
+-- первый период
 SET @Period1_1 = dbo.fGetIDPeriodMY(@Month1_1, @Year1_1);
 SET @Period1_2 = dbo.fGetIDPeriodMY(@Month1_2, @Year1_2);
-
+-- второй период
 SET @Period2_1 = dbo.fGetIDPeriodMY(@Month2_1, @Year2_1);
 SET @Period2_2 = dbo.fGetIDPeriodMY(@Month2_2, @Year2_2);
 
 
-
-
-
-
 SELECT
-  c.Account
- ,s.name + ', ' + LTRIM(STR(h.housenumber)) + ISNULL(h.housenumberchar, '') + '-' + LTRIM(a.flat) AS Address 
+  c.Account,
+  s.name + ', ' + LTRIM(STR(h.housenumber)) + ISNULL(h.housenumberchar, '') + '-' + LTRIM(a.flat) AS Address 
 INTO #tmpAccount
-FROM Contract c
-    ,GObject g
-    ,Person p
-    ,Address a
-    ,Street s
-    ,House h
+FROM Contract AS c,
+     GObject  AS g,
+     Person   AS p,
+     Address  AS a,
+     Street   AS s,
+     House    AS h
 WHERE c.IDContract = g.IDContract
-AND c.IDPerson = p.IDPerson
-AND p.IDAddress = a.IDAddress
-AND a.IDStreet = s.IDStreet
-AND a.IDHouse = h.IDHouse
+  AND c.IDPerson = p.IDPerson
+  AND p.IDAddress = a.IDAddress
+  AND a.IDStreet = s.IDStreet
+  AND a.IDHouse = h.IDHouse
 ORDER BY c.Account;
 
 SELECT
   c.Account,
   SUM(fu.FactAmount) AS sumP1
 INTO #tmpPotreblenie1
-FROM FactUse fu
-    ,TypeFU tf
-    ,Contract c
-    ,GObject g
+FROM FactUse  AS fu,
+     TypeFU   AS tf,
+     Contract AS c,
+     GObject  AS g
 WHERE tf.IDTypeFU = fu.IDTypeFU
-AND fu.IDGObject = g.IDGObject
-AND c.IDContract = g.IDContract
-AND fu.IDPeriod >= @Period1_1
-AND fu.IDPeriod <= @Period1_2
---AND g.IDStatusGObject = 1
-AND fu.IDTypeFU IN (1, 2)   -- 1 по ПУ, 2 - по кол-ву проживающих
+  AND fu.IDGObject = g.IDGObject
+  AND c.IDContract = g.IDContract
+  AND fu.IDPeriod >= @Period1_1
+  AND fu.IDPeriod <= @Period1_2
+  --AND g.IDStatusGObject = 1
+  AND fu.IDTypeFU IN (1, 2)   -- 1 по ПУ, 2 - по кол-ву проживающих
 GROUP BY c.Account
 ORDER BY c.Account;
 
@@ -86,17 +83,17 @@ SELECT
   c.Account,
   SUM(fu.FactAmount) AS sumP2
 INTO #tmpPotreblenie2
-FROM FactUse fu
-    ,TypeFU tf
-    ,Contract c
-    ,GObject g
+FROM FactUse  AS fu,
+     TypeFU   AS tf,
+     Contract AS c,
+     GObject  AS g
 WHERE tf.IDTypeFU = fu.IDTypeFU
-AND fu.IDGObject = g.IDGObject
-AND c.IDContract = g.IDContract
-AND fu.IDPeriod >= @Period2_1
-AND fu.IDPeriod <= @Period2_2
---AND g.IDStatusGObject = 1
-AND fu.IDTypeFU IN (1, 2)
+  AND fu.IDGObject = g.IDGObject
+  AND c.IDContract = g.IDContract
+  AND fu.IDPeriod >= @Period2_1
+  AND fu.IDPeriod <= @Period2_2
+  --AND g.IDStatusGObject = 1
+  AND fu.IDTypeFU IN (1, 2)
 GROUP BY c.Account
 ORDER BY c.Account;
 
@@ -128,21 +125,28 @@ SELECT DISTINCT
   ISNULL(p2.sumP2, 0) AS [0721г-0622г] 
 -- ,ISNULL(p.summ3, 0) AS [0721г-0622г]
 INTO #tmpItog
-FROM #tmpAccount as a
-LEFT JOIN #tmpPotreblenie1 as p1
-  ON a.Account = p1.Account
-LEFT JOIN #tmpPotreblenie2 as p2
-  ON a.Account = p2.Account
---LEFT JOIN #tmpPotreblenie3 p3
---  ON a.Account = p3.Account
+FROM #tmpAccount AS a
+  LEFT JOIN #tmpPotreblenie1 AS p1 ON a.Account = p1.Account
+  LEFT JOIN #tmpPotreblenie2 AS p2 ON a.Account = p2.Account
+--LEFT JOIN #tmpPotreblenie3 p3 ON a.Account = p3.Account
 ORDER BY a.Account;
 
-IF @Potreblenie = 0 
+IF @Potreblenie = 0
   BEGIN
   -- с нулевым потреблением
+  -- но оставляем с 0 только там где ОУ и ПУ подключены 
   SELECT
     ROW_NUMBER() OVER(ORDER BY i.Account ASC) AS Row#,*
-  FROM #tmpItog i
+  FROM #tmpItog AS i
+  WHERE 
+    (i.Account IN (SELECT c.Account
+                          FROM Contract AS c
+                            JOIN GObject AS  g  ON c.IDContract = g.IDContract
+                            JOIN GMeter  AS  g1 ON g.IDGObject  = g1.IDGObject
+                          WHERE g.IDStatusGObject = 1 AND g1.IDStatusGMeter = 1)
+    ) 
+    OR (i.[0721г-0622г]<>0)
+    OR (i.[2021г] <> 0)
   --WHERE i.[0721г-0622г] <> 0
   --OR i.[2021г] <> 0
   --OR i.[2020г] <> 0
@@ -152,12 +156,12 @@ ELSE
   BEGIN
     SELECT
       ROW_NUMBER() OVER(ORDER BY i.Account ASC) AS Row#,*
-    FROM #tmpItog i
+    FROM #tmpItog AS i
     WHERE i.[0721г-0622г] <> 0
       OR i.[2021г] <> 0
       --OR i.[2020г] <> 0
     ORDER BY i.Account;
-  END  
+  END
  
 ---------------------------------------------------------
 
