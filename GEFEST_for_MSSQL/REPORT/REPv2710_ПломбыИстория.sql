ac0cef2a2e0c0636a434b332ec2330d8d55e1e91
Mikhail Chmir <Mchmir@mail.ru>
2022-10-28 00:28:37 +0600

    addFile(SQL) History Plomb

---
+++ GEFEST_for_MSSQL/REPORT/REPv2710_ПломбыИстория.sql
@@ -0,0 +1,16 @@
+﻿SELECT
+       gph.PlombNumber1,
+       LEFT(gph.PlombNumber1, 4) AS Seria,
+       gph.DatePlomb
+  FROM GMeterPlombHistory AS gph
+ WHERE NOT gph.PlombNumber1 IS NULL
+
+    UNION
+
+SELECT
+       gph.PlombNumber2,
+       LEFT(gph.PlombNumber2, 4) AS Seria,
+       gph.DatePlomb
+  FROM GMeterPlombHistory AS gph
+ WHERE NOT gph.PlombNumber2 IS NULL
+ ORDER BY 1
