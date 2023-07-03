7d60df7c1085d4a0fb2cc103d3910a4629a19afe
Mikhail Chmir <mchmir@mail.ru>
2023-07-03 13:13:33 +0600

    updateFile(PY) обновление файла поиска ошибок в транспортниках

--- PyCode/MSSQL/parse/errorInPackage.py
+++ PyCode/MSSQL/parse/errorInPackage.py
@@ -1,14 +1,18 @@
-in_file = r"C:\Users\User\Desktop\Посылки из горгаз 2023\Горгаз №590.txt"
+import os.path
 
-lst = ['upd', 'ins', 'if ', 'del']
+in_file = r"C:\Users\User\Desktop\Посылки из горгаз 2023\Горгаз №607.txt"
 
-with open(in_file, 'r') as read_file:
-    n = 0
-    for line in read_file:
-        n += 1
-        my_string = line.strip()
-        if my_string[0:3] not in lst:
-            print('проверьте строки: ', my_string[0:20], '...под номером: ', n)
+lst = ['upd', 'ins', 'if ', 'del', 'dbc', 'beg', 'com', 'dec']
 
+if os.path.exists(in_file):
+    with open(in_file, 'r') as read_file:
+        n = 0
+        for line in read_file:
+            n += 1
+            my_string = line.strip()
+            if my_string[0:3].lower() not in lst:
+                print('проверьте строки: ', my_string[0:20], '...под номером: ', n)
+else:
+    print('Файл не существует:', in_file)
 
 # print("Разделитель ascii-код {}, ascii-символ {}".format(str(ord('¶')), chr(182)))
